21084916003712abdb8a36c168a29eef4b454f76
Lucas Montel Costa <aimpostor6@gmail.com>
2023-01-02 11:07:35 -0300

    tela-de-login

---
+++ Login/assets/js/script.js
@@ -0,0 +1,11 @@
+ let inputUser = document.getElementById("usuario");
+ let inputPass = document.getElementById("senha");
+ let botao = document.querySelector("button");
+ 
+
+
+ function limpa() {
+ inputPass.value = "";
+ inputUser.value = "";
+ }
+
